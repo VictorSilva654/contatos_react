3eb6ecd38b494e4258858a0bfde1a1c5c662c769
VictorSilva654 <victordejesus654@gmail.com>
2023-07-19 20:33:18 -0300

    commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import GlobalStyle from './styles'
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <h1>Oie</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/styles/index.ts
@@ -0,0 +1,16 @@
+import styled, { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+
+
+  @import url('https://fonts.googleapis.com/css2?family=Tektur&display=swap');
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Tektur', cursive;
+  }
+`
+
+export default GlobalStyle
